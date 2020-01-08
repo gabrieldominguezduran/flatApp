f4da89ba7160fba63c153f9aa72b90d046513f0c
Gabriel <gdominguez@synergie.es>
2020-01-08 14:13:37 +0100

    change Search form again

--- app/javascript/components/SearchForm.js
+++ app/javascript/components/SearchForm.js
@@ -25,23 +25,53 @@ class SearchForm extends Component {
     constructor(){
       super()
       this.state = {
-        search : ''
+        flat: [],
+        sortedFlats: [],
+        city: "All",
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        minRooms: 0,
+        maxRooms: 0,
+        minBath: 0,
+        maxBath: 0
+
       }
   }
+  //Get data
+  componentDidMount() {
+    let flats = this.props.modules(data)
+    let maxPrice = Math.max(...flats.map(data => data.price))
+    let maxSize = Math.max(...flats.map(data => data.m2))
+    
+    
 
-  updateSearch(event) {
-    this.setState({search: event.target.value})
+    this.setState({
+      flats,
+      allFlats,
+      sortedFlats: flats,
+      price: maxPrice,
+      maxPrice,
+      maxSize
+    })
+
+  }
+  
+  updateSearch = event => {
+    const type = event.target.type
+    const name = event.target.name
+    const value = event.target.value
   }
 
-  render(){
-    const { search } = this.state 
-    const filtered = this.props.modules.filter( (data) => {
-      
-        return search !== "" && data.city.toLowerCase().indexOf( search.toLowerCase()) === -1 
-          
-        
-      
-    })
+  filterFlats = () => {
+    console.log("hello")
+    
+  }
+
+  render() {
+    
       return (
           <div>
             <Input type="text" 
